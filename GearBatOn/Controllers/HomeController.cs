6fe2b692e22c5fee655ad3a14aac832b9356aa63
DongDev <admin@dongdev.com>
2021-07-22 23:21:48 +0700

    Them xem danh sach san pham

--- GearBatOn/Controllers/HomeController.cs
+++ GearBatOn/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace GearBatOn.Controllers
 {
     public class HomeController : Controller
     {
+        Paging pg = new Paging();
         GearBatOnContext _dbContext = new GearBatOnContext();
         public ActionResult Index()
         {
@@ -23,14 +24,15 @@ namespace GearBatOn.Controllers
             return View(product);
         }
 
-        public ActionResult ListProduct()
+        public ActionResult ListProduct(int? id)
         {
+            ViewBag.categoryId = id;
             return View();
         }
 
         public ActionResult PartialHomeProduct(int id)
         {
-            List<Product> products = _dbContext.Products.Where(x => x.CategoryId == id).OrderBy(x => x.Id).Skip(0).Take(10).ToList();
+            List<Product> products = _dbContext.Products.Where(x => x.CategoryId == id && x.Status == true).OrderBy(x => x.Id).Skip(0).Take(10).ToList();
             foreach (var item in products)
             {
                 var temp = _dbContext.Images.FirstOrDefault(x => x.ProductId == item.Id);
@@ -40,6 +42,25 @@ namespace GearBatOn.Controllers
                 }
             }
             return PartialView("PartialHomeProduct", products);
+        }
+
+        public ActionResult PartialListProduct(int id, int? page)
+        {
+            if (page == null) page = 1;
+            int take = 10;
+            int total = _dbContext.Products.Where(x => x.CategoryId == id && x.Status == true).Count();
+            List<Product> products = _dbContext.Products.Where(x => x.CategoryId == id && x.Status == true).OrderBy(x => x.Id).Skip(((int)page - 1) * take).Take(take).ToList();
+            foreach (var item in products)
+            {
+                var temp = _dbContext.Images.FirstOrDefault(x => x.ProductId == item.Id);
+                if (temp != null)
+                {
+                    item.FeatureImage = temp.ImagePath;
+                }
+            }
+            ViewBag.Paging = pg.Pagination(total, (int)page, take);
+
+            return PartialView("PartialListProduct", products);
         }
     }
 }
